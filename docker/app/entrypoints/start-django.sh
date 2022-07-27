85d59cffe47308b3c922a9d37bb9f62e32d7a504
franciscorode <fran_rode@hotmail.com>
2022-07-27 20:45:38 +0200

    🔧 chore: use dev web server on local to enable auto-reload

--- docker/app/entrypoints/start-django.sh
+++ docker/app/entrypoints/start-django.sh
@@ -6,8 +6,13 @@ python3.9 manage.py migrate --noinput
 
 # Start app
 echo "Starting application"
-gunicorn src.config.asgi:application \
-    -k uvicorn.workers.UvicornWorker \
-    --bind :8000 \
-    --log-level error \
-    --timeout 30
+if [ "${DEPLOY_ENVIRONMENT:-x}" = "LOCAL" ]; then
+    python3.9 manage.py runserver 0.0.0.0:8000
+else
+    gunicorn src.config.asgi:application \
+        -k uvicorn.workers.UvicornWorker \
+        --bind :8000 \
+        --log-level error \
+        --timeout 30
+        --reload
+fi
